c8ef604caa7cbe80d642c88f30a1391d51a032ec
CarlosCCF94 <carlosccf94@gmail.com>
2026-01-28 12:56:49 -0600

    Practica Modulo 5
    Relaice un script llamado hola mundo, edite el script con las funciones: LogError, LogWarning y Log para mostrar distintos tipos de logs en Unity.

---
+++ ProyectoInicialEBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    int x;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        x = 0;
+        print("algo paso");
+        
+        Debug.LogWarning("algo salio mediamente mal");
+        Debug.LogError("algo salio mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // x = x + 1;
+        // Debug.Log(x);
+
+        Debug.Log("Hola desde Update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.LogWarning("Hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto ha sido habilitado");
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inabilitado");
+    }
+}
